493b6dbff2c26e5aee1e9774f82763a191371fbf
unknown <abdc5307@gmail.com>
2026-08-19 23:22:43 +0900

    feat: 깃허브 액션 자동 배포 구축 및 배포 설정 업데이트

--- mcm_from_me/mcm_from_me/settings.py
+++ mcm_from_me/mcm_from_me/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'frontend']
+STATIC_ROOT = BASE_DIR / 'static'
 
 # [추가] 프론트엔드 연동용 CORS 전체 허용
 CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
