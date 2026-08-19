d9ea3e193cd3afb5c073ec71c254f86a61c782c0
yulmoo75 <yulmoo0705@gmail.com>
2026-08-19 16:40:48 +0900

    feat:추천 사진

--- mcm_from_me/ai_story/models.py
+++ mcm_from_me/ai_story/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="제품명")
     description = models.TextField(verbose_name="제품 설명")
+    image_url = models.URLField(max_length=500, null=True, blank=True, verbose_name="제품 이미지 URL")
 
     def __str__(self):
         return self.name
